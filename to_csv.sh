cd66a1593ce9e80e3960f89ab06a2a79ed6f445b
Sergio Vavassori <sergio.vavassori@deveryware.com>
2020-03-26 21:06:10 +0100

    Use date as ISO8601

--- to_csv.sh
+++ to_csv.sh
@@ -19,7 +19,7 @@ cat $TEXT_OUT | tail -n +$FIRST_LINE | head -n 200 > $INFO
 
 # keep first 20 lines (CCAA) and only the ones starting with a number
 # delete dots from "thousands" (e.g. 3.453 -> 3453)
-# substitute comas with dots (e.g.  6,44 -> 6.44)
+# substitute comas with dots   (e.g.  6,44 -> 6.44)
 cat $INFO | awk '(NR<=20) || /^[0-9]/' | sed -e 's/\([0-9]\)\./\1/g' -e 's/,/./g' > $CLEANED
 
 # read all info into array for easier lookup
@@ -27,7 +27,7 @@ IFS=$'\t' read -r -a arr <<< $(cat $CLEANED | tr '\n' '\t')
 
 # write CSV file
 MOD_DATE=$(pdfinfo -isodates ${FILE_IN}| awk '/ModDate/ { print $2}')
-TIME=$(date --date=$MOD_DATE --rfc-3339=seconds | sed 's/+.\+$//g')
+TIME=$(date --date=$MOD_DATE --iso-8601=seconds | sed 's/+.\+$//g')
 DATE=$(date --date=$MOD_DATE --iso-8601)
 [[ $# -eq 2 ]] && FILE_OUT="$2" || FILE_OUT="datos-ccaa-${DATE}.csv"
 
