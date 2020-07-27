0b2daaaed85316a709a069fb653941793ff5f5dd
Sergio Vavassori <vavassori@gmail.com>
2020-07-27 12:56:50 +0200

    Uses new format version

--- download.sh
+++ download.sh
@@ -1,32 +1,31 @@
-# !/bin/bash
+#!/bin/bash
 
-YESTERDAY=$(date --date=yesterday +%F)
-FILE_CCAA="datos-ccaa.csv"
-FILE_ESP="datos-españa.csv"
-FILE="agregados.csv"
-URL="https://cnecovid.isciii.es/covid19/resources/${FILE}"
-OPTS="--no-verbose --output-document=${FILE}"
+# download updated files
+URLS=(
+	"https://cnecovid.isciii.es/covid19/resources/datos_ccaas.csv"
+	"https://cnecovid.isciii.es/covid19/resources/datos_provincias.csv"
+	"https://www.mscbs.gob.es/profesionales/saludPublica/ccayes/alertasActual/nCov-China/documentos/Fallecidos_COVID19.xlsx"
+)
 
-cd "csv/"
-wget $OPTS $URL
+OPTS="--no-verbose --timestamping --directory-prefix=csv"
 
-# delete carriage return
-# delete 'NOTA' lines at end
-# sums "PCR+" and "TestAc+" if present, otherwise uses "casos" field
-# rearrange date field to be first
-# format date as YYYY-MM-DD
+for url in "${URLS[@]}"
+do
+	wget ${OPTS} ${url}
+done
+
+CURRENT=$(find pdf/ -name "*.pdf" | sed 's/.\+_\([0-9]\+\)_.\+/\1/g' | sort --numeric-sort --unique | tail -n 1)
+NEXT=$((CURRENT+1))
+wget --no-verbose --directory-prefix=pdf/ "https://www.mscbs.gob.es/profesionales/saludPublica/ccayes/alertasActual/nCov-China/documentos/Actualizacion_${NEXT}_COVID-19.pdf"
+
+
 # replace CCAA codes with full name
-cat $FILE | tr -d '\r' \
-          | awk '/^[A-Z][A-Z],[0-9]/ || NR <=1 { print $0}' \
-          | awk 'BEGIN { FS=OFS="," } \
-                 { if (NR <= 1) { print $0, "TotalPositivos" } \
-                   else { totalPositive=$4+$5; print $0, totalPositive } \
-                 }' \
-          | awk 'BEGIN { FS=OFS="," } { print $2, $1, $3, $4, $5, $6, $7, $8, $9 }' \
-          | sed --regexp-extended \
-                -e 's|([0-9]+)/([0-9]+)/([0-9]+)|\3-\2-\1|g' \
-                -e 's/-([0-9])-/-0\1-/g' \
-                -e 's/-([0-9]),/-0\1,/g' \
+FILE="csv/datos_ccaas.csv"
+FILE_ESP="csv/casos-españa.csv"
+FILE_DEATHS="csv/Fallecidos_COVID19.csv"
+DEATHS="csv/Fallecidos_COVID19.xlsx"
+TMP="tmp"
+cat $FILE | sed --regexp-extended \
                 -e 's/AN,/Andalucía,/g' \
                 -e 's/AR,/Aragón,/g' \
                 -e 's/AS,/Asturias,/g' \
@@ -46,17 +45,38 @@ cat $FILE | tr -d '\r' \
                 -e 's/NC,/Navarra,/g' \
                 -e 's/PV,/País Vasco,/g' \
                 -e 's/RI,/La Rioja,/g' \
-          > $FILE_CCAA
+          > $FILE_ESP
 
-# create country file
-QUERY=" SELECT FECHA, 'España' AS CCAA, SUM(CASOS) AS CASOS, "
-QUERY+='SUM("PCR+") AS "PCR+", SUM("TestAc+") AS "TestAc+",'
-QUERY+="SUM(Hospitalizados) AS Hospitalizados, SUM(UCI) AS UCI,"
-QUERY+="SUM(Fallecidos) AS Fallecidos,"
-QUERY+="SUM(TotalPositivos) AS TotalPositivos "
-QUERY+="FROM - "
-QUERY+="GROUP BY FECHA ORDER BY FECHA"
+# create 'cases' file
+QUERY_CASOS="SELECT 'España' AS España, fecha, SUM(num_casos) AS casos, "
+QUERY_CASOS+="SUM(num_casos_prueba_pcr) AS pcr, "
+QUERY_CASOS+="SUM(num_casos_prueba_test_ac) AS testAc, "
+QUERY_CASOS+="SUM(num_casos_prueba_otras) AS otras, "
+QUERY_CASOS+="SUM(num_casos_prueba_desconocida) AS desconocida "
+QUERY_CASOS+="FROM - "
+QUERY_CASOS+="GROUP BY fecha "
+cat $FILE_ESP | q --skip-header --delimiter=, "$QUERY_CASOS" >> $FILE_ESP
 
-cat $FILE_CCAA | q --skip-header --output-header --delimiter=, "$QUERY" \
-               | sed 's/\.0//g' \
-               > $FILE_ESP
+# sort by date without header and then add the new header
+tail --lines=+2 $FILE_ESP | sort --key=2 --field-separator=, --output $FILE_ESP
+sed -i '1 i\localidad,fecha,casos,pcr,testAc,otras,desconocida' $FILE_ESP
+
+# create cumulative deaths file
+# and changes the date format
+libreoffice --headless --convert-to "csv:Text - txt - csv (StarCalc):44,,76,1" --outdir csv $DEATHS
+head --lines=-1 $FILE_DEATHS | awk --field-separator ',' \
+    '{if (NR==1) { print $0 }
+      else {
+        { printf "%s",$1 }
+        for (i=2; i<=NF; i++) {
+          ccaa[i]+=$i;
+          { printf ",%d", ccaa[i] }
+        }
+        print ""
+      }
+    }' \
+    | sed --regexp-extended \
+          -e 's| / CCAA||g' \
+          -e 's|^([0-9]+)/([0-9]+)/([0-9]+)|\3-\2-\1|g' \
+    > $TMP
+mv $TMP $FILE_DEATHS
