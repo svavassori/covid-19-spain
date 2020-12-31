ce243eb81bafa400a8681df37e9a07277dfc813b
Sergio Vavassori <vavassori@gmail.com>
2020-12-31 18:01:29 +0100

    Uses new files since previous have been removed from the site

--- download.sh
+++ download.sh
@@ -2,9 +2,12 @@
 
 # download updated files
 URLS=(
-	"https://cnecovid.isciii.es/covid19/resources/datos_ccaas.csv"
-	"https://cnecovid.isciii.es/covid19/resources/datos_provincias.csv"
-	"https://www.mscbs.gob.es/profesionales/saludPublica/ccayes/alertasActual/nCov/documentos/Datos_Casos_COVID19.csv"
+      "https://cnecovid.isciii.es/covid19/resources/casos_diagnostico_ccaa.csv"
+      "https://cnecovid.isciii.es/covid19/resources/casos_diagnostico_provincia.csv"
+      "https://cnecovid.isciii.es/covid19/resources/casos_diag_ccaadecl.csv"
+      "https://cnecovid.isciii.es/covid19/resources/casos_hosp_uci_def_sexo_edad_provres.csv"
+      "https://cnecovid.isciii.es/covid19/resources/metadata_ccaa_prov_res.pdf"
+      "https://cnecovid.isciii.es/covid19/resources/metadata_ccaadecl_prov_edad_sexo.pdf"
 )
 
 OPTS="--no-verbose --timestamping --directory-prefix=csv"
@@ -14,99 +17,36 @@ do
 	wget ${OPTS} ${url}
 done
 
-# convert from ISO-8859-14 to UTF-8
-DATA_FILE="csv/Datos_Casos_COVID19.csv"
-if file --brief $DATA_FILE | grep --quiet "ISO-8859"; then
-	iconv --from-code=ISO-8859-1 --to-code=UTF-8 $DATA_FILE | sponge $DATA_FILE
-fi
-
 CURRENT=$(find pdf/ -name "*.pdf" | sed 's/.\+_\([0-9]\+\)_.\+/\1/g' | sort --numeric-sort --unique | tail -n 1)
 NEXT=$((CURRENT+1))
 wget --no-verbose --directory-prefix=pdf/ "https://www.mscbs.gob.es/profesionales/saludPublica/ccayes/alertasActual/nCov-China/documentos/Actualizacion_${NEXT}_COVID-19.pdf"
 
 
-# replace CCAA codes with full name
-FILE="csv/datos_ccaas.csv"
+# replace province codes with CC.AA. full name and calculate aggregated values
+# removes the 'NC' lines since they are not assigned to any CC.AA.
+FILE="csv/casos_hosp_uci_def_sexo_edad_provres.csv"
 FILE_ESP="csv/casos-españa.csv"
-cat $FILE | sed --regexp-extended \
-                -e 's/AN,/Andalucía,/g' \
-                -e 's/AR,/Aragón,/g' \
-                -e 's/AS,/Asturias,/g' \
-                -e 's/IB,/Baleares,/g' \
-                -e 's/CN,/Canarias,/g' \
-                -e 's/CB,/Cantabria,/g' \
-                -e 's/CM,/Castilla La Mancha,/g' \
-                -e 's/CL,/Castilla y León,/g' \
-                -e 's/CT,/Cataluña,/g' \
-                -e 's/CE,/Ceuta,/g' \
-                -e 's/VC,/C. Valenciana,/g' \
-                -e 's/EX,/Extremadura,/g' \
-                -e 's/GA,/Galicia,/g' \
-                -e 's/MD,/Madrid,/g' \
-                -e 's/ML,/Melilla,/g' \
-                -e 's/MC,/Murcia,/g' \
-                -e 's/NC,/Navarra,/g' \
-                -e 's/PV,/País Vasco,/g' \
-                -e 's/RI,/La Rioja,/g' \
+
+QUERY_PROV="SELECT ccaa, fecha, SUM(num_casos) AS num_casos, "
+QUERY_PROV+="SUM(num_hosp) AS num_hosp, "
+QUERY_PROV+="SUM(num_uci) AS num_uci, "
+QUERY_PROV+="SUM(num_def) AS num_def "
+QUERY_PROV+="FROM - "
+QUERY_PROV+="GROUP BY ccaa, fecha "
+cat $FILE | grep -v "^NC" \
+          | sed --file=province.sed \
+          | q --skip-header --output-header --delimiter=, "$QUERY_PROV" \
           > $FILE_ESP
 
-# create 'cases' file
+# calculate aggregated values for Spain
 QUERY_CASOS="SELECT 'España' AS España, fecha, SUM(num_casos) AS casos, "
-QUERY_CASOS+="SUM(num_casos_prueba_pcr) AS pcr, "
-QUERY_CASOS+="SUM(num_casos_prueba_test_ac) AS testAc, "
-QUERY_CASOS+="SUM(num_casos_prueba_otras) AS otras, "
-QUERY_CASOS+="SUM(num_casos_prueba_desconocida) AS desconocida "
+QUERY_CASOS+="SUM(num_hosp) AS hospitalizados, "
+QUERY_CASOS+="SUM(num_uci) AS uci, "
+QUERY_CASOS+="SUM(num_def) AS fallecidos "
 QUERY_CASOS+="FROM - "
 QUERY_CASOS+="GROUP BY fecha "
 cat $FILE_ESP | q --skip-header --delimiter=, "$QUERY_CASOS" >> $FILE_ESP
 
 # sort by date without header and then add the new header
 tail --lines=+2 $FILE_ESP | sort --key=2 --field-separator=, --output $FILE_ESP
-sed -i '1 i\localidad,fecha,casos,pcr,testAc,otras,desconocida' $FILE_ESP
-
-
-# This file is supposed-to-be the one they use to create
-# the PDF, but some data like current (not new daily)
-# hospitalized or ICU are missing while present in the PDF.
-#
-# * it removes the first 7 lines full of comments
-# * changes ; with ,
-# * reformat date from dd-mm-yyyy to yyyy-mm-dd
-# * calculate totals for Spain
-FILE_PDF_ESP="csv/datos-pdf-españa.csv"
-tail -n +7 "${DATA_FILE}" | \
-      sed --regexp-extended \
-          -e 's/;/,/g' \
-          -e 's/([0-9]+)-([0-9]+)-([0-9]+)/\3-\2-\1/g' \
-          -e 's/ES-AN,/Andalucía,/g' \
-          -e 's/ES-AR,/Aragón,/g' \
-          -e 's/ES-AS,/Asturias,/g' \
-          -e 's/ES-IB,/Baleares,/g' \
-          -e 's/ES-CN,/Canarias,/g' \
-          -e 's/ES-CB,/Cantabria,/g' \
-          -e 's/ES-CM,/Castilla La Mancha,/g' \
-          -e 's/ES-CL,/Castilla y León,/g' \
-          -e 's/ES-CT,/Cataluña,/g' \
-          -e 's/ES-CE,/Ceuta,/g' \
-          -e 's/ES-VC,/C. Valenciana,/g' \
-          -e 's/ES-EX,/Extremadura,/g' \
-          -e 's/ES-GA,/Galicia,/g' \
-          -e 's/ES-MD,/Madrid,/g' \
-          -e 's/ES-ML,/Melilla,/g' \
-          -e 's/ES-MC,/Murcia,/g' \
-          -e 's/ES-NC,/Navarra,/g' \
-          -e 's/ES-PV,/País Vasco,/g' \
-          -e 's/ES-RI,/La Rioja,/g' \
-      > $FILE_PDF_ESP
-
-QUERY_PDF="SELECT 'España' AS España, Fecha, SUM(Casos_Diagnosticados) AS casos, "
-QUERY_PDF+="SUM(Hospitalizados) AS hospitalizados, SUM(UCI) AS uci, "
-QUERY_PDF+="SUM(Fallecidos) AS fallecidos "
-QUERY_PDF+="FROM - "
-QUERY_PDF+="GROUP BY Fecha ORDER BY Fecha"
-
-cat $FILE_PDF_ESP | q --delimiter=, --skip-header "$QUERY_PDF" >> $FILE_PDF_ESP
-
-# sort by date without header and then add the new header
-tail --lines=+2 $FILE_PDF_ESP | sort --key=2 --field-separator=, --output $FILE_PDF_ESP
-sed -i '1 i\localidad,fecha,casos,hospitalizados,uci,fallecidos' $FILE_PDF_ESP
+sed -i '1 i\localidad,fecha,casos,hospitalizados,uci,fallecidos' $FILE_ESP
---
+++ province.sed
@@ -0,0 +1,54 @@
+s/^provincia_iso,/ccaa,/g
+s/^AL,/Andalucía,/g
+s/^CA,/Andalucía,/g
+s/^CO,/Andalucía,/g
+s/^GR,/Andalucía,/g
+s/^H,/Andalucía,/g
+s/^J,/Andalucía,/g
+s/^MA,/Andalucía,/g
+s/^SE,/Andalucía,/g
+s/^HU,/Aragón,/g
+s/^TE,/Aragón,/g
+s/^Z,/Aragón,/g
+s/^O,/Asturias,/g
+s/^S,/Cantabria,/g
+s/^AV,/Castilla y León,/g
+s/^BU,/Castilla y León,/g
+s/^LE,/Castilla y León,/g
+s/^P,/Castilla y León,/g
+s/^SA,/Castilla y León,/g
+s/^SG,/Castilla y León,/g
+s/^SO,/Castilla y León,/g
+s/^VA,/Castilla y León,/g
+s/^ZA,/Castilla y León,/g
+s/^AB,/Castilla La Mancha,/g
+s/^CR,/Castilla La Mancha,/g
+s/^CU,/Castilla La Mancha,/g
+s/^GU,/Castilla La Mancha,/g
+s/^TO,/Castilla La Mancha,/g
+s/^GC,/Canarias,/g
+s/^TF,/Canarias,/g
+s/^B,/Cataluña,/g
+s/^GI,/Cataluña,/g
+s/^L,/Cataluña,/g
+s/^T,/Cataluña,/g
+s/^BA,/Extremadura,/g
+s/^CC,/Extremadura,/g
+s/^C,/Galicia,/g
+s/^LU,/Galicia,/g
+s/^OR,/Galicia,/g
+s/^PO,/Galicia,/g
+s/^PM,/Baleares,/g
+s/^MU,/Murcia,/g
+s/^M,/Madrid,/g
+s/^NA,/Navarra,/g
+s/^VI,/País Vasco,/g
+s/^BI,/País Vasco,/g
+s/^SS,/País Vasco,/g
+s/^LO,/La Rioja,/g
+s/^A,/C. Valenciana,/g
+s/^CS,/C. Valenciana,/g
+s/^V,/C. Valenciana,/g
+s/^CE,/Ceuta,/g
+s/^ML,/Melilla,/g
+s/^ME,/Melilla,/g
