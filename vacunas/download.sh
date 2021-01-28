b939b9e09574c59566e5cde291f3499ba5506255
Sergio Vavassori <vavassori@gmail.com>
2021-01-28 20:24:36 +0100

    Adds vaccines information

---
+++ vacunas/download.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+FILE_PDF="Informe_GIV_comunicacion_$(date +%+4Y%m%d).pdf"
+OPTS="--no-verbose --timestamping --directory-prefix=informes"
+URL="https://www.mscbs.gob.es/profesionales/saludPublica/ccayes/alertasActual/nCov/documentos/"
+wget ${OPTS} "${URL}${FILE_PDF}"
+
+# extract PDF modifcation date
+DATE=$(date +%F --date=$(pdfinfo -isodates informes/${FILE_PDF} | awk '/ModDate:/ { print $2}'))
+
+# extract data only for CC.AA. list, should be 6 columns:
+# CC.AA. name | delivered Pfizer | delivered Moderna | total delivered | administered | vaccinated
+# change separator from space to ',' to help identify columns
+# removes dot (thousands) from numbers
+# change ',' to '.' from percent
+# fixes CC.AA. names
+java -jar tika-app-1.25.jar --text "informes/${FILE_PDF}" \
+    | perl -p0e 's/Castilla La \n\nMancha\n/Castilla La Mancha /g' \
+    | grep --file=pattern_ccaa \
+    | sed -e 's/ (\*)//g' \
+          -e 's/\([a-z0-9]\) \([0-9]\)/\1,\2/g' \
+          -e 's/\([0-9]\)\.\([0-9]\)/\1\2/g' \
+          -e 's/\([0-9]\),\([0-9][0-9]\?%\) /\1.\2,/' \
+          -e 's/Baleares/Islas Baleares/g' \
+          -e 's/Leon/León/g' \
+          -e 's/Castilla La Mancha/Castilla-La Mancha/g' \
+          -e 's/C. Valenciana/Comunidad Valenciana/g' \
+    | awk -F ',' '{ print $1","$2","$3","$5","$7}' \
+    | python3 to_json.py ${DATE} - data/
+
+# merge daily with cumulative ones
+DATA_FILES=("administered" "delivered" "vaccinated")
+for file in ${DATA_FILES[@]}
+do
+    jq --null-input '[ inputs ] | flatten' "data/regions_${file}.json" "data/${DATE}_${file}.json" | sponge "data/regions_${file}.json"
+    tail --lines=+2 "data/${DATE}_${file}.csv" >> "data/regions_${file}.csv"
+done
+
+# create summary files for Spain
+for file in ${DATA_FILES[@]}
+do
+    jq '[ group_by(.date, .supplier)
+        | .[]
+        | { date: .[0] | .date,
+            iso_code: "ES",
+            nuts1: "ES",
+            name: "España",
+            supplier: .[0] | .supplier, '"\
+            ""${file} : ( map(.${file})"' | reduce .[] as $sum (0; . + $sum) ) } ]' \
+            "data/regions_${file}.json" \
+        > "data/state_${file}.json"
+    cat "data/regions_${file}.csv" \
+    | q --skip-header --output-header --delimiter=, \
+        "SELECT date, 'ES' AS iso_code, 'ES' AS nuts, 'España' AS name, supplier, sum(${file}) AS ${file} \
+         FROM - GROUP BY date, supplier" \
+    > "data/state_${file}.csv"
+done
+
+# removes temporary files
+rm "data/${DATE}_"*
---
+++ vacunas/to_json.py
@@ -0,0 +1,79 @@
+import sys
+import csv
+import json
+
+class Data:
+    name_to_codes = {}
+
+    def load_iso_codes(self):
+        with open("codes_spain.csv") as code_file:
+            csv_reader = csv.DictReader(code_file)
+            for row in csv_reader:
+                self.name_to_codes[row["name"]] = row
+
+    def new_element(self, date, ccaa_name):
+        codes = self.name_to_codes[ccaa_name]
+        element = {}
+        element['date'] = date
+        element["iso_code"] = codes["iso3611-2"]
+        element["nuts2"] = codes["NUTS2"]
+        element["name"] = ccaa_name
+        element["supplier"] = "Unknown"
+        return element
+
+def create_json(date, lines, output_dir):
+    administered = []
+    delivered = []
+    vaccinated = []
+    suppliers = [(1, "Pfizer/BioNTech"),
+                 (2, "Moderna")]
+    csv_reader = csv.reader(lines)
+    data = Data()
+    data.load_iso_codes()
+    for row in csv_reader:
+        element = data.new_element(date, row[0])
+        element["administered"] = int(row[3])
+        administered.append(element)
+        element = data.new_element(date, row[0])
+        element["vaccinated"] = int(row[4])
+        element["supplier"] = suppliers[0][1]
+        vaccinated.append(element)
+        for column, supplier in suppliers:
+            element = data.new_element(date, row[0])
+            element["delivered"] = int(row[column])
+            element["supplier"] = supplier
+            delivered.append(element)
+    for (name, data_list) in [("administered", administered),
+                              ("delivered", delivered),
+                              ("vaccinated", vaccinated)]:
+        output_file = output_dir + date + "_" + name
+        write_json(output_file + ".json", data_list)
+        write_csv(output_file + ".csv", data_list)
+
+def write_json(file_name, content):
+    with open(file_name, "w", encoding="utf8") as output_file:
+        json.dump(content, output_file, ensure_ascii=False, indent=4)
+
+def write_csv(file_name, content):
+    with open(file_name, "w", encoding="utf-8", newline="") as output_file:
+        csv_writer = csv.DictWriter(output_file, fieldnames=content[0])
+        csv_writer.writeheader()
+        csv_writer.writerows(content)
+
+def parseArgs(args):
+    date = sys.argv[1]
+    lines = None
+    if sys.argv[2] == "-":
+        # read lines from stdin
+        lines = sys.stdin.readlines()
+    else:
+        with open(sys.argv[2]) as input_file:
+            lines = input_file.readlines()
+    output_dir = "./" if len(sys.argv) == 3 else sys.argv[3]
+    if not output_dir.endswith("/"):
+        output_dir += "/"
+    return date, lines, output_dir
+
+if len(sys.argv) > 2:
+    date, lines, output_dir = parseArgs(sys.argv)
+    create_json(date, lines, output_dir)
